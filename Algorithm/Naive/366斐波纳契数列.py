d16a7f5fa40fa4a066f6de2e30a3d78966195fc5
Martin_Yan <Maitong_y@163.com>
2018-07-02 16:28:58 +0800

    add the first question

---
+++ Algorithm/Naive/366斐波纳契数列.py
@@ -0,0 +1,14 @@
+class Solution:
+    """
+    @param: n: an integer
+    @return: an ineger f(n)
+    """
+    def fibonacci(self, n):
+        num=[0,1]
+        if n == 1:
+            return 0
+        elif n==2:
+            return 1
+        for i in range(0,n-2):
+            num.append(num[-1] + num[-2])
+        return num[-1]
